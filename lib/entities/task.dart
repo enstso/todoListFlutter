51eb72067994bdfada9f8f7ff8b9dabf7e0ed507
Enstso <enstso@outlook.fr>
2025-09-02 16:50:16 +0200

    adapt the Task class

--- lib/entities/task.dart
+++ lib/entities/task.dart
@@ -2,35 +2,16 @@ class Task {
   final String id;
   final String title;
   final String description;
-  bool isCompleted;
+  final bool isCompleted;
   final DateTime createdAt;
-  DateTime? completedAt;
+  final DateTime? completedAt;
 
-  Task(
-    this.id, 
-    this.title, 
-    this.description,
-      {
-        this.isCompleted = false,
-         DateTime? createdAt,
-          this.completedAt})
-          : createdAt = createdAt ?? DateTime.now();
-
-Task copyWith({
-    String? id,
-    String? title,
-    String? description,
-    bool? isCompleted,
-    DateTime? createdAt,
-    DateTime? completedAt,
-  }) {
-    return Task(
-      id ?? this.id,
-      title ?? this.title,
-      description ?? this.description,
-      isCompleted: isCompleted ?? this.isCompleted,
-      createdAt: createdAt ?? this.createdAt,
-      completedAt: completedAt ?? this.completedAt,
-    );
-  }
+   Task({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.isCompleted,
+    required this.createdAt,
+    this.completedAt,
+   });
 }
