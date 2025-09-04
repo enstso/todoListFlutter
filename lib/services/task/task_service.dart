1abbff1f7dea644cb1ed98571ec0c72f3007a998
Enstso <enstso@outlook.fr>
2025-09-04 10:58:20 +0200

    adapt my class task_service for to relate to a user

--- lib/services/task/task_service.dart
+++ lib/services/task/task_service.dart
@@ -1,24 +1,38 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:todo_list/models/task_model.dart';
 
 class TaskService {
-  final CollectionReference taskRef = FirebaseFirestore.instance.collection('tasks');
+  final _auth = FirebaseAuth.instance;
+  final CollectionReference _taskRef = FirebaseFirestore.instance.collection('tasks');
 
   Future<void> addTask(TaskModel task) async {
-    await taskRef.add(task.toMap());
+    final uid = _auth.currentUser?.uid;
+    if (uid == null) {
+      throw Exception('User not authenticated');
+    }
+    await _taskRef.add({...task.toMap(), 'userId': uid});
   }
 
   Future<void> updateTask(TaskModel task) async {
-    await taskRef.doc(task.id).update(task.toMap());
+     final uid = _auth.currentUser?.uid;
+    if (uid == null) {
+      throw Exception('User not authenticated');
+    }
+    await _taskRef.doc(task.id).update({...task.toMap(), 'userId': uid});
   }
 
   Future<void> deleteTask(String id) async {
-    await taskRef.doc(id).delete();
+    final uid = _auth.currentUser?.uid;
+    if (uid == null) {
+      throw Exception('User not authenticated');
+    }
+    await _taskRef.doc(id).delete();
   }
 
   Future<TaskModel?> getTaskById(String id) async {
-    final doc = await taskRef.doc(id).get();
+    final doc = await _taskRef.doc(id).get();
     if (doc.exists) {
       return TaskModel.fromMap(doc.data(), doc.id);
     }
@@ -26,7 +40,11 @@ class TaskService {
   }
 
   Stream<List<TaskModel>> getTasks() {
-    return taskRef.orderBy('createdAt',descending: true).snapshots().map((snapshot) {
+    final uid = _auth.currentUser?.uid;
+    if (uid == null) {
+      throw Exception('User not authenticated');
+    }
+    return _taskRef.where('userId', isEqualTo: uid).orderBy('createdAt', descending: true).snapshots().map((snapshot) {
       return snapshot.docs.map((doc) {
         return TaskModel.fromMap(doc.data(), doc.id);
       }).toList();
